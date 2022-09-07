318723f523dce708b8df98ed41b51c9750a461e9
Olivier Ory <olivier.ory@uliege.be>
2022-09-07 17:31:04 +0200

    Reverse-String: Attempt 1

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn reverse(input: &str) -> String {
-  let mut output = String::new();
+    let mut output = String::new();
     for c in String::from(input).chars().rev() {
-      output.push(c);
+        output.push(c);
     }
     output
 }
