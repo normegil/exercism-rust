d72bbfe163e0989e05eac4cc7b154d9790a3cfaa
Olivier Ory <mail@normegil.be>
2022-09-07 22:03:12 +0200

    Reverse String: Remove logging

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,7 +1,5 @@
 pub fn reverse(input: &str) -> String {
     let mut output = String::new();
-    println!("{}", String::from(input));
-    println!("{:?}", String::from(input).chars());
     for c in String::from(input).chars().rev() {
         output.push(c);
     }
