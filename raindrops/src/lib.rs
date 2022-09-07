b98d71028635027e97bd273de635786b5ef67971
Olivier Ory <mail@normegil.be>
2022-09-07 22:23:11 +0200

    Raindrops - Solved

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut output = String::new();
+    if n % 3 != 0 && n % 5 != 0 && n % 7 != 0 {
+        return n.to_string();
+    }
+    if n % 3 == 0 {
+        output.push_str("Pling");
+    }
+    if n % 5 == 0 {
+        output.push_str("Plang");
+    }
+    if n % 7 == 0 {
+        output.push_str("Plong");
+    }
+    return output;
 }
