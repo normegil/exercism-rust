c2acd0cb619fd792992a523e5d37e42c649d3c67
Olivier Ory <mail@normegil.be>
2022-09-28 22:22:08 +0200

    Assembly-Line: First solution

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -2,10 +2,20 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+const CARS_PER_HOUR: i32 = 221;
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let speed_converted: i32 = speed as i32;
+    let perfect_cars_per_hours = (CARS_PER_HOUR * speed_converted) as f64;
+    if speed < 5 {
+        return perfect_cars_per_hours;
+    } else if speed < 9 {
+        return perfect_cars_per_hours * 0.9;
+    } else {
+        return perfect_cars_per_hours * 0.77;
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    return (production_rate_per_hour(speed) / 60_f64) as u32
 }
