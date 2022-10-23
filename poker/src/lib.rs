c99a0d532aeccbde7b003e8705b3debba7808141
Olivier Ory <mail@normegil.be>
2022-10-23 14:58:32 +0200

    Poker

--- poker/src/lib.rs
+++ poker/src/lib.rs
@@ -1,7 +1,147 @@
+use std::cmp::Ordering;
+
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
+    for hand in hands {
+        Hand::new(hand);
+    }
+
     unimplemented!("Out of {:?}, which hand wins?", hands)
+}
+
+#[derive(PartialEq, Eq)]
+enum Suit {
+    Hearts,
+    Spades,
+    Clubs,
+    Diamonds
+}
+
+#[derive(PartialEq, Eq)]
+struct Card {
+    suit: Suit,
+    value: Value
+}
+
+impl Card {
+    fn new(representation: &str) -> Result<Card, String> {
+        let nb_chars = representation.chars().count();
+        let value: String = representation.chars().take(nb_chars - 1).collect();
+        let value = match Value::new(&value) {
+            Ok(val) => val,
+            Err(e) => return Err(e)
+        };
+
+        let suit_repr = representation.chars().last().unwrap();
+        match suit_repr {
+            'H' => Result::Ok(Card { suit: Suit::Hearts, value: value }),
+            'S' => Result::Ok(Card { suit: Suit::Spades, value: value }),
+            'C' => Result::Ok(Card { suit: Suit::Clubs, value: value }),
+            'D' => Result::Ok(Card { suit: Suit::Diamonds, value: value }),
+            _ => Result::Err(format!("'{}' suit not recognized", suit_repr))
+        }
+    }
+}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.value.cmp(&other.value)
+    } 
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    } 
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+enum Value {
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Ten,
+    Nine,
+    Eight,
+    Seven,
+    Six,
+    Five,
+    Four,
+    Three,
+    Two
+}
+
+impl Value {
+    fn new(representation: &str) -> Result<Value, String> {
+        match representation {
+            "1" => Result::Ok(Value::Ace),
+            "K" => Result::Ok(Value::King),
+            "Q" => Result::Ok(Value::Queen),
+            "J" => Result::Ok(Value::Jack),
+            "10" => Result::Ok(Value::Ten),
+            "9" => Result::Ok(Value::Nine),
+            "8" => Result::Ok(Value::Eight),
+            "7" => Result::Ok(Value::Seven),
+            "6" => Result::Ok(Value::Six),
+            "5" => Result::Ok(Value::Five),
+            "4" => Result::Ok(Value::Four),
+            "3" => Result::Ok(Value::Three),
+            "2" => Result::Ok(Value::Two),
+            _ => Result::Err(format!("'{}' value not recognized", representation)) 
+        }
+    }
+}
+
+enum Combination {
+    high_card(Suit),
+    one_pair,
+    two_pair,
+    three_of_a_kind,
+    straight,
+    flush,
+    full_house,
+    four_of_a_kind,
+    straight_flush,
+    five_of_a_kind,
+}
+
+struct Hand<'a> {
+    representation: &'a str,
+    hand: Vec<Card>
+}
+
+impl<'a> Hand<'a> {
+    fn new(representation: &str) -> Result<Hand, String> {
+        let mut hand: Vec<Card> = Vec::new();
+        for card in representation.split_whitespace() {
+            match Card::new(card) {
+                Ok(suit) => hand.push(suit),
+                Err(e) => return Err(e)
+            }    
+        }
+
+        hand.sort();
+
+        Result::Ok(Hand { representation, hand })
+    }
+
+    // fn best_hand(&self) -> Combination {
+    //     if self.is_five_of_a_kind() {
+    //         return Combination::five_of_a_kind;
+    //     } else {
+    //         return 
+    //     }
+    // }
+
+    fn is_five_of_a_kind(&self) -> bool {
+        let x = &self.hand[0];
+        self.hand.iter().all(|y| y == x)
+    }
+
+    // fn highest_card(&self) -> bool {
+    // }
 }
