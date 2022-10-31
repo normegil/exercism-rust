5ce3a2c7981fa96dbff60864b6511fc673f8f349
Olivier Ory <mail@normegil.be>
2022-10-31 09:53:54 +0100

    Allergies: Done

--- allergies/src/lib.rs
+++ allergies/src/lib.rs
@@ -1,6 +1,8 @@
-pub struct Allergies;
+pub struct Allergies {
+    score: u32
+}
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Allergen {
     Eggs,
     Peanuts,
@@ -12,22 +14,50 @@ pub enum Allergen {
     Cats,
 }
 
+impl Allergen {
+    fn as_array() -> Vec<Allergen> {
+        vec![
+            Allergen::Eggs,
+            Allergen::Peanuts,
+            Allergen::Shellfish,
+            Allergen::Strawberries,
+            Allergen::Tomatoes,
+            Allergen::Chocolate,
+            Allergen::Pollen,
+            Allergen::Cats
+        ]
+    }
+}
+
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        unimplemented!(
-            "Given the '{}' score, construct a new Allergies struct.",
-            score
-        );
+        Allergies { score }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        unimplemented!(
-            "Determine if the patient is allergic to the '{:?}' allergen.",
-            allergen
-        );
+        match allergen {
+            Allergen::Eggs => self.check_flag(0), 
+            Allergen::Peanuts => self.check_flag(1), 
+            Allergen::Shellfish => self.check_flag(2), 
+            Allergen::Strawberries => self.check_flag(3),
+            Allergen::Tomatoes => self.check_flag(4), 
+            Allergen::Chocolate => self.check_flag(5), 
+            Allergen::Pollen => self.check_flag(6),
+            Allergen::Cats => self.check_flag(7),
+        }
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        let mut allergies_list: Vec<Allergen> = Vec::new();
+        for allergen in Allergen::as_array().iter() {
+            if self.is_allergic_to(allergen) {
+                allergies_list.push(*allergen)
+            }
+        }
+        allergies_list
+    }
+
+    fn check_flag(&self, flag_nb: u8) -> bool {
+        ((1 << flag_nb) & self.score) != 0
     }
 }
