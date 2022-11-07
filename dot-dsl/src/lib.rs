d49abfcdf79cfe7ec290bdfb59843770038740ac
Olivier Ory <mail@normegil.be>
2022-11-07 23:25:19 +0100

    DotDSL: Test

--- dot-dsl/src/lib.rs
+++ dot-dsl/src/lib.rs
@@ -1,9 +1,43 @@
 pub mod graph {
-    pub struct Graph;
+    use self::graph_items::node::Node;
+
+    pub struct Graph {
+        pub nodes: Vec<Node>,
+        pub edges: Vec<Node>,
+        pub attrs: Vec<Node>,
+    }
 
     impl Graph {
         pub fn new() -> Self {
-            unimplemented!("Construct a new Graph struct.");
+            Graph { nodes: Vec::new(), edges: Vec::new(), attrs: Vec::new() }
+        }
+
+        pub fn with_nodes(mut self, nodes: &[Node]) -> Self {
+            for n in nodes {
+                self.nodes.push(*n)
+            }
+            self
+        }
+    }
+
+    pub mod graph_items {
+        pub mod edge {
+            pub struct Edge {
+
+            }
+        }
+
+        pub mod node {
+            #[derive(Clone, Copy, PartialEq, Eq)]
+            pub struct Node {
+                data: str
+            }
+
+            impl Node {
+                pub fn new(_: &str) -> Node {
+                    Node {  }
+                }
+            }
         }
     }
 }
