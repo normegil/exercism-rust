8423c0cf5046af31f07f1f0fa39aeaccc6a51ece
Olivier Ory <mail@normegil.be>
2022-11-09 20:49:28 +0100

    Queen Attack: Done

--- queen-attack/src/lib.rs
+++ queen-attack/src/lib.rs
@@ -1,31 +1,42 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition {
+    rank: i32,
+    file: i32
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    position: ChessPosition
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return None.",
-            rank,
-            file
-        );
+        if rank < 0 || file < 0 || rank > 7 || file > 7 {
+            return Option::None;
+        }
+        Option::Some(ChessPosition { rank, file })
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!(
-            "Given the chess position {:?}, construct a Queen struct.",
-            position
-        );
+        Queen { position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!(
-            "Determine if this Queen can attack the other Queen {:?}",
-            other
-        );
+        let self_file = self.position.file;
+        let other_file = other.position.file; 
+        let self_rank = self.position.rank;
+        let other_rank = other.position.rank; 
+        if self_file == other_file || self_rank == other_rank {
+            true
+        } else {
+            let file_diff = self_file - other_file;
+            if self_rank == other_rank + file_diff || self_rank == other_rank - file_diff {
+                true
+            } else {
+                false
+            }
+        }
     }
 }
